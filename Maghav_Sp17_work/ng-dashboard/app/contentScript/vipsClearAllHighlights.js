5a5cbe70405e23dff2e875af2c8499a5941800ee
Zubin <zpahuja2@illinois.edu>
2017-06-25 02:18:32 -0500

    switch branches

--- Maghav_Sp17_work/ng-dashboard/app/contentScript/vipsClearAllHighlights.js
+++
@@ -1,6 +0,0 @@
-console.log("VIPS Clear All Highlights");
-vips = new VipsAPI();
-globalBlocks = vips.getVisualBlockList();
-for (var i = 0; i < globalBlocks.length; i++) {
-	globalBlocks[i]['-att-box'].style.border = "";
-}
