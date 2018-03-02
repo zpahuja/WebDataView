9c73153861bf918e5cbc6d3fa821ed0152f02950
Zubin Pahuja <zubin@Zubin.local>
2018-03-02 13:12:30 -0600

    test commit

--- ng-dashboard/app/contentScript/experiments/clearAllHighlights.js
+++
@@ -1,8 +0,0 @@
-console.log("VIPS Clear All Highlights");
-
-vips = new VipsAPI();
-globalBlocks = vips.getVisualBlockList();
-
-for (var i = 0; i < globalBlocks.length; i++) {
-	globalBlocks[i]['-att-box'].style.border = "";
-}
